d9b6afdb28e51314ea41d917faada4a5ff17979f
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-12-05 23:16:24 +0100

    convert a string to number

---
+++ AreYouPlayingBanjo/script.js
@@ -0,0 +1,30 @@
+/*
+
+Are You Playing Banjo?
+
+DESCRIPTION:
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo"
+name + " does not play banjo"
+
+Names given are always valid strings.
+
+*/
+
+//1
+function areYouPlayingBanjo(name) {
+    return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+  }
+
+  //2
+  function areYouPlayingBanjo(name) {
+    if (name[0].toLowerCase() === 'r') {
+      return name + ' plays banjo';
+    } else {
+      return name + ' does not play banjo';
+    }
+  }
